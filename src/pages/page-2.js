f4905b1a5b6dfb9ec613442849a63a7c53eaacb7
Enea <eneasecondary@gmail.com>
2018-09-01 12:01:00 -0700

    Update to second page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
-    <p>Thank you in advance for the opportunity! Please also consider my good friend Roger Pender: https://compassionate-roentgen-be66ad.netlify.com/</p>
+    <p>Thank you in advance for the opportunity! Please also consider my good friend  <a href = 'https://compassionate-roentgen-be66ad.netlify.com/'>Roger Pender</a></p>
     <Link to="/">home</Link>
   </div>
 )
